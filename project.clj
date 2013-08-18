8334e9b30b664d831396db53a169a52064b58b94
Christian Schröder <christian.schroeder@conceptboard.com>
2013-08-18 13:08:26 +0200

    playing a bit with leipzig

--- project.clj
+++ project.clj
@@ -5,5 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [ohpauleez/clojure_leap "0.1.1-SNAPSHOT"]
-                 [overtone "0.8.1" :exclusions [org.clojure/clojure]]]
+                 [overtone "0.8.1" :exclusions [org.clojure/clojure]]
+                 [leipzig "0.6.0"]]
   :repl-options {:init-ns leap-overtone-sandbox.core})
--- src/leap_overtone_sandbox/core.clj
+++ src/leap_overtone_sandbox/core.clj
@@ -1,8 +1,14 @@
 (ns leap-overtone-sandbox.core
   (:require [clojure-leap.core :as leap]
             [clojure-leap.screen :as l-screen]
-            [clojure-leap.gestures :as gestures])
-  (:use     [overtone.live]))
+            [clojure-leap.gestures :as gestures]
+            [overtone.live :as ot])
+  (:use     overtone.inst.sampled-piano
+            leipzig.melody
+            leipzig.scale
+            leipzig.chord
+            leipzig.canon
+            leipzig.live))
 
 (def default-controller (atom nil))
 (def listeners (atom []))
@@ -24,10 +30,29 @@
 (defn shutdown []
   (doall listeners #(leap/remove-listener! @default-controller %)))
 
+(def hungarian-minor (scale (ot/resolve-scale :hungarian-minor)))
+(def melody
+  (->>
+    (phrase [ 1 1 2 1 1]
+            [ 0 2 1 1 -1.5])
+    (where :part (is :melody))))
 
-(definst theremin [freq 220] (saw freq))
+(defmethod play-note :melody [{midi :pitch}] (sampled-piano midi))
+
+(comment defmethod play-note :melody [{midi :pitch}] (ot/ctl theremin :freq (ot/midi->hz)))
+
+(defn play-melody
+  []
+  (->> melody
+    (times 2)
+    (where :time (bpm 90))
+    (where :duration (bpm 90))
+    (where :pitch (comp C hungarian-minor))
+    play))
+
+(ot/definst theremin [freq 220] (ot/saw freq))
 (comment 
   defn process-frame [controller frame screens] 
   (when-let [pointable (and (leap/pointables? frame) (first (leap/pointables frame)))] 
     (let [position-map (l-screen/intersect-position screens pointable)] 
-      (ctl theremin :freq (min 600 (max 0 (:x position-map))))))))
+      (ot/ctl theremin :freq (min 600 (max 0 (:x position-map)))))))
